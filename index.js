893679209b56129cb760c3f7144307f893d672e6
Charder001 <rick.chardet@hva.nl>
2020-04-06 15:20:06 +0200

    followed socket chat tutorial

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
+const app = require("express")()
 const express = require("express")
 const port = process.env.PORT || 3000
-const app = express()
+var http = require('http').createServer(app)
+var io = require('socket.io')(http)
+
 
 app.set("view engine", "ejs")
 app.set("views", "views")
 
 app.use(express.static("public"))
 
-app.get("/", function(req, res){
+app.get('/', function(req, res){
     res.render("home")
-})
+});
 
-app.listen(port, function(){
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+      });
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+      });
+  });
+
+http.listen(port, function(){
     console.log(`Application started on port: ${port}`)
 })
