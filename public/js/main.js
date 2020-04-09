630da7ecc2aa0c6a56e09475c9b37178c2cad42e
Charder001 <rick.chardet@hva.nl>
2020-04-09 16:19:59 +0200

    added random word functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,23 @@
 // //tutorial source: https://socket.io/get-started/chat/
 // const currentWord = document.querySelector('#current-word');
-
+const words = ["Kaas", "Stoomwals", "Roomboter", "Jesse Klaver"]
 
 // Pick & show random word
-function showWord(words) {
+
   // Generate random array index
-  const randIndex = Math.floor(Math.random() * words.length);
-  console.log(words[randIndex])
-}
+  var randomWord = words[Math.floor(Math.random() * words.length)];
+  document.querySelector("#currentWord").innerHTML = randomWord
+
 
   var socket = io();
   document.querySelector('form').addEventListener("submit", function (e) {
     e.preventDefault(); // prevents page reloading
     let inputText = document.querySelector("#m").value
-    if (inputText.includes("Kaas")) {
+    if (inputText.includes(randomWord)) {
     socket.emit('chat message', document.querySelector("#m").value);
     document.getElementById('m').value = "";
     } else {
-      console.log("nope!")
+      document.querySelector('#messages').insertAdjacentHTML("beforeend", `Your sentence must include ${randomWord}!`);
     }
   });
   socket.on('chat message', function (msg) {
