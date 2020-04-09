9e17a139ad08ddb1954aab4f491f25feee3e9eb9
Charder001 <rick.chardet@hva.nl>
2020-04-09 15:56:16 +0200

    added sentence checking

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,24 @@
 // //tutorial source: https://socket.io/get-started/chat/
-// var socket = io()
 // const currentWord = document.querySelector('#current-word');
-// const words = ["aardappel", "kaas", "stoomwals", "zeehond", "pasen"]
 
-// // Pick & show random word
-// function showWord(words) {
-//   // Generate random array index
-//   const randIndex = Math.floor(Math.random() * words.length);
-//   console.log(words[randIndex])
-// }
 
-// var socket = io()
-// document.querySelector("form").submit(function(e){
-//   e.preventDefault()
-//   socket.emit("chat message", document.querySelector("#m").val())
-//   document.querySelector("#m").val("")
-//   return false
-// }) 
-
-// socket.on("chat message", function(msg){
-//   document.querySelector("#messages").append(document.querySelector("<li>").text(msg))
-// })
-
+// Pick & show random word
+function showWord(words) {
+  // Generate random array index
+  const randIndex = Math.floor(Math.random() * words.length);
+  console.log(words[randIndex])
+}
 
   var socket = io();
   document.querySelector('form').addEventListener("submit", function (e) {
     e.preventDefault(); // prevents page reloading
+    let inputText = document.querySelector("#m").value
+    if (inputText.includes("Kaas")) {
     socket.emit('chat message', document.querySelector("#m").value);
     document.getElementById('m').value = "";
-    // return false;
+    } else {
+      console.log("nope!")
+    }
   });
   socket.on('chat message', function (msg) {
     console.log(msg)
