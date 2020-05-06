682d3903ca7b33ed21a0944a4d8beb44bb6aa368
Charder001 <rick.chardet@hva.nl>
2020-05-06 13:54:06 +0200

    added server side timer and ready counter

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ var io = require('socket.io')(http)
 let score = 0
 const words = ["You're gonna need a bigger boat", "How nice of you"]
 let randomWord;
+var playerCount = 0
+var readyCount = 0
+var countdown = 10;
+
 
 app.set("view engine", "ejs")
 app.set("views", "views")
-
 app.use(express.static("public"))
 
 app.get('/', function (req, res) {
@@ -22,6 +25,8 @@ app.get('/test', function (req, res) {
 
 
 io.on('connection', function (socket) {
+    playerCount ++
+    console.log("there are " + playerCount + " players")
     io.emit('random word', randomWord)
     socket.on('chat message', function (msg) {
         console.log('message: ' + msg);
@@ -37,12 +42,46 @@ io.on('connection', function (socket) {
         console.log(`User ` + socket.username + ' connected');
         io.emit("user Connected", socket.username)
     });
-    socket.on('disconnect', function (username) {
-        socket.username = username;
+    socket.on('disconnect', function () {
         console.log(`User ` + socket.username + ' disconnected');
         io.emit("user Disconnected", socket.username)
+        playerCount --
+        console.log("there are " + playerCount + " players")
     });
+    socket.on('newRandomString', function(){
+        randomize(words)
+        io.emit('random word', randomWord)
+        console.log(randomWord)
+    })
+    socket.on('ready', function(){
+        readyCount ++
+        console.log(readyCount, playerCount)
+        io.emit("readyCountUpdatedPlus", readyCount)
+        if (readyCount == playerCount){
+            console.log("game starting")
+            setInterval(myTimer, 1000)
+        } 
+    })
+    socket.on('unready', function(){
+        readyCount --
+        console.log(readyCount, playerCount)
+        countdown = 10;
+        io.sockets.emit('timer', { countdown: countdown });
+        clearInterval(myTimer)
+        io.emit("readyCountUpdatedMinus", readyCount)
+    })
 });
+
+function myTimer() {
+    countdown--;
+    io.sockets.emit('timer', { countdown: countdown });
+    if (countdown == 0) {
+        randomize(words)
+        io.emit('random word', randomWord)
+        console.log(randomWord)
+      countdown = 10;
+  }
+};
 
 
 
@@ -57,3 +96,5 @@ function randomize(words) {
 http.listen(port, function () {
     console.log(`Application started on port: ${port}`)
 })
+
+
--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,7 @@ var socket = io();
 var wordsInSentence
 var width = 1;
 var username = prompt('Please tell me your name');
+var display = document.querySelector('#time');
 socket.emit('username', username)
 
 socket.on('random word', function (randomWord) {
@@ -19,37 +20,37 @@ socket.on('random word', function (randomWord) {
 socket.on('user Connected', function (username) {
     console.log(username)
     document.querySelector('#messages').insertAdjacentHTML("beforeend", `<li>${username} has connected</li>`);
-  });
+});
 
-//   socket.on('user Disconnected', function (username) {
-//     document.querySelector('#messages').insertAdjacentHTML("beforeend", `<li>${username} has disconnected</li>`);
-//   });
-
-socket.on('disconnect', function (username) {
-    document.querySelector('#messages').insertAdjacentHTML("beforeend", `<li>${socket.username} has disconnected</li>`);
-  });
-
+socket.on('user Disconnected', function (usernamed) {
+    document.querySelector('#messages').insertAdjacentHTML("beforeend", `<li>${usernamed} has disconnected</li>`);
+});
 
 
 socket.on('Done', function (score) {
-  console.log(score)
-  document.querySelector('#messages').insertAdjacentHTML("beforeend", `<li>You are finished</li>`);
-  document.querySelector('#messages').insertAdjacentHTML("beforeend", `<li>Your score: ${score}</li>`);
+    console.log(score)
+    document.querySelector('#messages').insertAdjacentHTML("beforeend", `<li>Your score: ${score}</li>`);
 });
 
 socket.on('DoneToAll', function (score, username) {
     console.log(score)
     console.log(username)
-    document.querySelector('#messages').insertAdjacentHTML("beforeend", `<li>${username} is finished</li>`);
     document.querySelector('#messages').insertAdjacentHTML("beforeend", `<li>His score: ${score}</li>`);
-  });
+});
+
+socket.on('startTimer', function () {
+    console.log("game started")
+    startTimer(10, display)
+
+});
 
 
-
+//check input
 humanInput.addEventListener("input", function (changes) {
     if (changes.target.value == challengeString) {
         score++
         console.log(score)
+        document.querySelector("#typeField").reset()
         socket.emit('Score Up', score, username);
     } else if (changes.target.value && changes.target.value.length !== 0) {
         var currentHumanInput = changes.target.value;
@@ -71,12 +72,36 @@ humanInput.addEventListener("input", function (changes) {
     }
 })
 
+//countdown timer
+function startTimer(duration, display) {
+    var timer = duration,
+        minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
+        display.textContent = seconds;
 
+        if (--timer < 0) {
+            socket.emit('newRandomString');
+            timer = duration;
+        }
+    }, 1000);
+}
 
-// function move() {
-//     var elem = document.getElementById("myBar");
-//     width++;
-//     elem.style.width = width + "%";
-// }
+var readyButton = document.querySelector("#readyButton")
+readyButton.addEventListener("click", function () {
+    console.log(readyButton.innerText)
+    if (readyButton.innerText == "ready") {
+        socket.emit('ready')
+        readyButton.innerHTML = ``
+    } 
+})
+
+socket.on('timer', function (data) {
+    document.querySelector('#counter').innerHTML = data.countdown;
+});
+
